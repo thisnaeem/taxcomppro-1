8e1664aff4826ad0af6387a78d0fee74f66c98f6
naeem <naeemanjum076@gmail.com>
2026-07-17 19:24:45 +0500

    ok

--- app/admin/affiliate/page.tsx
+++ app/admin/affiliate/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";  
 import { Loader2 } from "lucide-react";
 import {
   Settings01Icon, UserGroupIcon, DollarCircleIcon, Tick02Icon,
