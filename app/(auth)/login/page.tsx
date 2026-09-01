47a093670cadeabe5a28724246d8f80967b200c8
naeem <beyondnaeem13@gmail.com>
2026-09-01 18:22:20 +0500

    refactor: clean up trailing whitespace in login page import statement

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import Link from "next/link"; 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState, useEffect, Suspense } from "react";
